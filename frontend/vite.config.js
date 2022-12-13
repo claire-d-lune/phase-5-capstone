8f8d275d91bb54744905b1d704d45eb3c0fa79c5
Claire Steinhoff <claire.steinhoff@gmail.com>
2022-12-13 11:03:10 -0700

    Fixed my rails setup.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
   server: {
     port: 3000, // changed my outgoing port to 3000 instead of the default 5173
     proxy: {
-        '/api': { // changed my proxy path to /api
-          target: 'http://127.0.0.1:4000/', // whenever we type /api in our fetch it will be recognized as this route
+        '/api': { 
+          target: 'http://127.0.0.1:4000/', 
           changeOrigin: true, 
-          secure: false, // handle security issues 
-          rewrite: (path) => path.replace(/^\/api/, "") // rewrite the path to remove the /api
+          secure: false, 
+          rewrite: (path) => path.replace(/^\/api/, "") // rewrite the path to remove the /api -- I think there's a prob with this line. I'll come back
       },
     },
   },
