ce7d2020235171efe25d8c91872302502b0c9c1a
Claire Steinhoff <claire.steinhoff@gmail.com>
2022-12-14 10:59:38 -0700

    Saving redo of backend and functionality I have created to help make my seed data.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,39 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [questions, setQuestions] = useState()
+
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=50&category=15&difficulty=hard&type=multiple")
+    .then(res => res.json())
+    .then(data => setQuestions(data))
+  }, []);
+
+  const formatQuestion = (object, counter) => {
+    return (
+      `vg_hard_${counter} = Question.create(category: "${object.category}", format: "${object.type}", question: "${object.question}", correct_answer: "${object.correct_answer}", difficulty: "${object.difficulty}", incorrect_answer_1: "${object.incorrect_answers[0]}", incorrect_answer_2: "${object.incorrect_answers[1]}", incorrect_answer_3: "${object.incorrect_answers[2]}")`
+      )
+  }
+
+
+  let myGoal
+  let infoStack 
+
+  if (questions !== undefined){
+    let counter = 0 
+    myGoal = questions.results.map( q => {
+    let response = formatQuestion(q, counter)
+    counter += 1
+    return response
+    })
+    infoStack = myGoal.map(element => <p>{element}</p>)
+  }
+
+  
+
 
   return (
     <div className="App">
@@ -17,6 +47,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+        <p>{infoStack}</p>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -27,6 +58,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
     </div>
   )
 }
