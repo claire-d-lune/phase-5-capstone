960ad3d3f0c496b290b5ab66bc96d2e632e0f5c0
Claire Steinhoff <claire.steinhoff@gmail.com>
2022-12-21 14:12:17 -0700

    Added sign on to the front end and fixed an issue on my backend that was causing a problem with the request.

--- backend/app/serializers/user_serializer.rb
+++ backend/app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :first_name, :last_name, :image_url
+  attributes :id, :username, :first_name, :last_name, :image_url, :password_digest
 end
