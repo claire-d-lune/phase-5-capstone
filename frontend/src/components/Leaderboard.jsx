b9eb4352fdc65b72f8df33998d2ecb149342e305
Claire Steinhoff <claire.steinhoff@gmail.com>
2023-01-06 11:33:46 -0700

    locking in working version for MVP presentations. There is still a lot I plan to add later.

--- frontend/src/components/Leaderboard.jsx
+++ frontend/src/components/Leaderboard.jsx
@@ -4,8 +4,20 @@ import { useQuery } from "@tanstack/react-query";
 
 const Leaderboard = () => {
 
+    const [leaderboard, setLeaderboard] = useState([])
     const { data: attemptData, isLoading } = useQuery(["attempts"])
     const { data: userData } = useQuery(['allUsers'], () => axios('api/users').then(res => res.data))
+
+    let scoreArray = userData?.map((user) => {
+        //tallying score
+        let totalScore = 0 
+        user.attempts.forEach((attempt) => {
+            totalScore = totalScore + attempt.score
+        })
+        let scoreData = {score: totalScore, userName: user.username, image: user.image_url}
+        return  scoreData
+    })
+
 
     if(isLoading){
         return <p className='fixed self-center' >Loading...</p>
